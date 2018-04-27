6e99540071a7aa2954f13e272263e47edbadf96c
vjfernandez <vjfernandez@users.noreply.github.com>
2018-04-27 20:17:56 +0200

    inicial

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./node_modules/codemirror/lib/codemirror.css">
+ 
+    <script src="./node_modules/codemirror/lib/codemirror.js"></script>
+    <script src="./node_modules/codemirror/mode/javascript/javascript.js"></script>
+    <title>Document</title>
+</head>
+<body>
+<textarea id ="mycode">
+hola
+</textarea>
+
+
+
+  <script>
+ var myTextarea = document.getElementById("mycode");
+ var editor = CodeMirror.fromTextArea(myTextarea, {
+    lineNumbers: true,
+    mode: 'javascript'
+  });
+
+</script>
+  
+</body>
+</html>
